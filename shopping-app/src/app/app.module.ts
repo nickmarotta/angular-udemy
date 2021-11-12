c0b84b687baca734e1d1d09fc89441cd3f5b0860
Nick Marotta <nmarotta@commercehub.com>
2021-11-12 16:19:53 -0500

    Completed Section 8: Services and Dependency Injection

--- shopping-app/src/app/app.module.ts
+++ shopping-app/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FormsModule } from '@angular/forms';
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeItemComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
